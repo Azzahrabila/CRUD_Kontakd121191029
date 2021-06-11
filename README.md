041272ea80b76398e94ab6d965e38e22dd43315c
Azzahrabilaa <azzahrazalzabila14@gmail.com>
2021-06-11 20:32:16 +0700

    tugas selesai CRUD

---
+++ README.md
@@ -0,0 +1,7 @@
+Nama : Azzahra Zalzabila AR
+
+NIM : D121191029
+
+Mata Kuliah : Pemrograman Aplikasi Mobile b
+
+Tugas CRUD
---
+++ app/build/generated/ap_generated_sources/debug/out/io/realm/com_d121191029_CRUD_model_ModelSiswaRealmProxyInterface.java
@@ -0,0 +1,11 @@
+package io.realm;
+
+
+public interface com_d121191029_CRUD_model_ModelSiswaRealmProxyInterface {
+    public int realmGet$id();
+    public void realmSet$id(int value);
+    public String realmGet$nama();
+    public void realmSet$nama(String value);
+    public String realmGet$alamat();
+    public void realmSet$alamat(String value);
+}
